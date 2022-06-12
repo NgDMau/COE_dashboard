ae26e3f303638826468db20cd0b39c41ff666e2d
Huynh.nguyen <>
2022-06-12 15:08:57 +0700

    confirm

--- src/pages/apps/index.jsx
+++ src/pages/apps/index.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { AppsWrapper } from "./styled";
 import { useNavigate } from "react-router-dom";
-
+import { Button, Modal } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 const AppsPage = () => {
   const navigate = useNavigate();
+  const { confirm } = Modal;
+  const showConfirm = () => {
+    confirm({
+      icon: <ExclamationCircleOutlined />,
+      content: <Button>Dự án đang phát triển</Button>,
+      onOk() {
+        console.log("OK");
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+
   return (
     <AppsWrapper>
       <div className="content">
         <div className="item blue" onClick={() => navigate("/dashboard")}>
           Bệnh viện thực hành nuôi con bằng sữa mẹ
         </div>
-        <div className="item yellow">Ngân hàng sữa mẹ</div>
-        <div className="item green">
+        <div className="item yellow" onClick={showConfirm}>
+          Ngân hàng sữa mẹ
+        </div>
+        <div className="item green" onClick={showConfirm}>
           Chương trình mục tiêu Quốc gia về Dân Tộc thiểu số
         </div>
       </div>
