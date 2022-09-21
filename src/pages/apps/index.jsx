a1af54633baefbee6c6edf9e715ee749655d7b36
huynh.nguyen <huynh.nguyen@amela.vn>
2022-09-21 11:52:10 +0700

    fix language

--- src/pages/apps/index.jsx
+++ src/pages/apps/index.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { AppsWrapper } from "./styled";
-import { useNavigate } from "react-router-dom";
-import { Button, Modal } from "antd";
-import { useTranslation } from "react-i18next";
-import { ExclamationCircleOutlined } from "@ant-design/icons";
+import React from 'react';
+import { AppsWrapper } from './styled';
+import { useNavigate } from 'react-router-dom';
+import { Button, Modal } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 
-import logo from "../../assets/brand/Logo_AnT.png";
-import MenuLanguage from "../menuLanguage";
+import logo from '../../assets/brand/Logo_AnT.png';
+import MenuLanguage from '../menuLanguage';
 
 const AppsPage = () => {
   const { t } = useTranslation();
@@ -15,26 +15,26 @@ const AppsPage = () => {
   const showConfirm = () => {
     confirm({
       icon: <ExclamationCircleOutlined />,
-      content: <Button>{t("development")}</Button>,
+      content: <Button>{t('development')}</Button>,
     });
   };
 
   return (
     <AppsWrapper>
       <MenuLanguage />
-      <div className="logo">
-        <img src={logo} alt="" />
+      <div className='logo'>
+        <img src={logo} alt='' />
       </div>
-      <div className="content">
-        <div className="item blue" onClick={() => navigate("/dashboard")}>
-          {t("app.breastfeeding")}
+      <div className='content'>
+        <div className='item blue' onClick={() => navigate('/dashboard')}>
+          {t('app.breastfeeding')}
         </div>
-        <div className="item yellow" onClick={showConfirm}>
-          {t("app.breastMilkBank")}
+        <div className='item yellow' onClick={showConfirm}>
+          {t('app.breastMilkBank')}
         </div>
-        {/* <div className="item green" onClick={showConfirm}>
-          {t("app.nationalMinorities")}
-        </div> */}
+        <div className='item green' onClick={showConfirm}>
+          {t('app.nationalMinorities')}
+        </div>
       </div>
     </AppsWrapper>
   );
