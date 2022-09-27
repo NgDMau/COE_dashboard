7873d2ba3d6d8add78bd85c4f971544acf32f830
Huynh.nguyen <>
2022-09-27 22:22:38 +0700

    Cập nhật hiển thị "Tài liệu tham khảo"

---
+++ src/components/document/listHospital/styled.js
@@ -0,0 +1,11 @@
+import { Button, Select } from "antd";
+import styled from "styled-components";
+
+export const ListHospitalWrapper = styled.div`
+  margin-top: 20px;
+`;
+export const TitleListHospital = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+  margin: 8px 0px;
+`;
