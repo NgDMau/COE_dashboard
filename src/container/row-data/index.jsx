09a9dbe8e4b117127b8b18c702837016e6e3fb84
Huynh.nguyen <>
2022-10-30 23:01:05 +0700

    table raw data

--- src/container/row-data/index.jsx
+++ src/container/row-data/index.jsx
@@ -4,16 +4,18 @@ import { RowDataWrapper } from "./styled";
 import { Spin, Table } from "antd";
 import { useState } from "react";
 import { linkApi } from "../../common/ngok";
+import { useSelector } from "react-redux";
 
 const RowData = React.memo(() => {
   const user = localStorage.getItem("user")
     ? JSON.parse(localStorage.getItem("user"))
     : null;
+  const citiesData = useSelector((state) => state.data.citiesData);
 
   const [isLoading, setIsLoading] = useState(false);
   const [rowData, setRowData] = useState([]);
   const [numEntries, setNumEntries] = useState({});
-
+  console.log("numEntriesnumEntriesnumEntries", numEntries);
   const getDataRow = async (page) => {
     if (isLoading) {
       return;
@@ -30,10 +32,12 @@ const RowData = React.memo(() => {
       .then((response) => response.json())
       .then((data) => {
         if (data?.status === "successful") {
+          console.log("data?.datadata?.data", data?.data);
           setRowData(data?.data?.object_list);
           setNumEntries({
             total: data?.data?.num_entries,
             numPages: data?.data?.num_pages,
+            page_number: data?.data?.page_number,
           });
         }
       })
@@ -57,21 +61,404 @@ const RowData = React.memo(() => {
     );
   }, [rowData]);
 
+  const colums = [
+    {
+      title: "Tỉnh",
+      dataIndex: "hosp_province",
+      key: "hosp_province",
+      render: (record) => {
+        return (
+          <div>
+            {citiesData?.find((element) => element.code === record)?.name || ""}
+          </div>
+        );
+      },
+    },
+    {
+      title: "Đơn vị thực hiện khảo sát",
+      dataIndex: "hosp_operator",
+      key: "hosp_operator",
+    },
+    {
+      title: "Tiêu chí khảo sát",
+      dataIndex: "hosp_criteria_name",
+      key: "hosp_criteria_name",
+    },
+    {
+      title: "Tên bệnh viện được khảo sát",
+      dataIndex: "hosp_hospname",
+      key: "hosp_hospname",
+    },
+    {
+      title: "Đơn vị thực hiện khảo sát",
+      dataIndex: "hosp_byt_syt",
+      key: "hosp_byt_syt",
+    },
+    {
+      title: "Tên của bà mẹ",
+      dataIndex: "minf_moname",
+      key: "minf_moname",
+    },
+    {
+      title: "Số điện thoại",
+      dataIndex: "minf_phone",
+      key: "minf_phone",
+    },
+    {
+      title: "q00",
+      dataIndex: "q00",
+      key: "q00",
+    },
+    {
+      title: "Quý",
+      dataIndex: "qrt",
+      key: "qrt",
+    },
+    {
+      title: "Năm sinh của trẻ",
+      dataIndex: "byear",
+      key: "byear",
+    },
+    {
+      title: "Tháng sinh của trẻ",
+      dataIndex: "bmon",
+      key: "bmon",
+    },
+    {
+      title: "q01",
+      dataIndex: "q_current_bf_q01",
+      key: "q_current_bf_q01",
+    },
+    {
+      title: "q01a",
+      dataIndex: "q_current_bf_q01a",
+      key: "q_current_bf_q01a",
+    },
+    {
+      title: "q01b",
+      dataIndex: "q_current_bf_q01b",
+      key: "q_current_bf_q01b",
+    },
+    {
+      title: "q02",
+      dataIndex: "q_cbf_q02",
+      key: "q_cbf_q02",
+    },
+    {
+      title: "q02a",
+      dataIndex: "q_cbf_q02a",
+      key: "q_cbf_q02a",
+    },
+    {
+      title: "q02b",
+      dataIndex: "q_cbf_q02b",
+      key: "q_cbf_q02b",
+    },
+    {
+      title: "q03",
+      dataIndex: "q_cbf_q03",
+      key: "q_cbf_q03",
+    },
+    {
+      title: "q03/1",
+      dataIndex: "q_cbf_q03_1",
+      key: "q_cbf_q03_1",
+    },
+    {
+      title: "q03a",
+      dataIndex: "q_cbf_q03a",
+      key: "q_cbf_q03a",
+    },
+    // {
+    //   title: "q04",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q05a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q05b",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07a/1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07a/2",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07a/9",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07b",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q03_1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q04_1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q04_1a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q04_1b",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q05_1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1a/1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1a/2",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1a/9",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1c",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1b",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1b/1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1b/2",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q06_1b/3",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1/1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1/2",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1/3",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1/4",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1/5",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1/6",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q07_1a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q08",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/2",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/3",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/4",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/5",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/7",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/8",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09a/9",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a/1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a/2",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a/3",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a/4",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a/7",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a/8",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09_1a/9",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q09b",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/1",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/2",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/3",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/4",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/5",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/6",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/9",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10/0",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q10a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q11",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q11a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q12",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q12a",
+    //   key: "action",
+    // },
+    // {
+    //   title: "q13a",
+    //   key: "action",
+    // },
+  ];
+
   return (
     <RowDataWrapper>
       <div>
         {arr ? (
           <Table
             className="table-row-data"
-            columns={arr}
+            columns={colums}
             dataSource={rowData}
             loading={isLoading}
             key={(recod) => recod.hosp_hospname}
             showSizeChanger={false}
             pagination={{
-              defaultPageSize: numEntries?.numPages,
+              defaultPageSize: 5,
               showSizeChanger: true,
               total: numEntries?.total,
+              current: numEntries?.page_number,
               pageSizeOptions: ["5"],
               showTotal: (total, range) => (
                 <div className="title-pagination">
