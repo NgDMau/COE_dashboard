a52e3a38236ac8c38cc08f840ca009a1f90b596d
Huynh.nguyen <>
2022-12-05 12:30:20 +0700

    translate

--- src/components/document/CreateForm/CreateFrom.jsx
+++ src/components/document/CreateForm/CreateFrom.jsx
@@ -1,67 +1,67 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState } from 'react';
-import { InboxOutlined } from '@ant-design/icons';
-import { Input, message, Row } from 'antd';
+import React, { useState } from "react";
+import { InboxOutlined } from "@ant-design/icons";
+import { Input, message, Row } from "antd";
 
-import { useEffect } from 'react';
-import TextArea from 'antd/lib/input/TextArea';
+import { useEffect } from "react";
+import TextArea from "antd/lib/input/TextArea";
 
-import { ButtonCustom } from '../../../container/document/styled';
-import { useTranslation } from 'react-i18next';
+import { ButtonCustom } from "../../../container/document/styled";
+import { useTranslation } from "react-i18next";
 import {
   DraggerFile,
   FormWrapper,
   Title,
   TitleUpdateDoc,
   UploadWrapper,
-} from '../../../container/FormInputData/styled';
+} from "../../../container/FormInputData/styled";
 
 const Createfrom = ({ selected, getDataDocument, setSelected }) => {
   const { t } = useTranslation();
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
   const [fileList, setFileList] = useState([]);
   const [uploading, setUploading] = useState(false);
-  const [docName, setDocName] = useState('');
-  const [docNote, setDocNote] = useState('');
+  const [docName, setDocName] = useState("");
+  const [docNote, setDocNote] = useState("");
 
   useEffect(() => {
-    setDocName('');
-    setDocNote('');
+    setDocName("");
+    setDocNote("");
     setFileList([]);
   }, []);
 
   const handleUpload = () => {
     var myHeaders = new Headers();
-    myHeaders.append('Authorization', 'Token ' + user?.token);
+    myHeaders.append("Authorization", "Token " + user?.token);
     myHeaders.append(
-      'Cookie',
-      'csrftoken=iBZFVxTK55EuqJtf8E8fQsPM7FPB9T9fvcwZd0p84fHQooETm1i99ycSF1NITwZn'
+      "Cookie",
+      "csrftoken=iBZFVxTK55EuqJtf8E8fQsPM7FPB9T9fvcwZd0p84fHQooETm1i99ycSF1NITwZn"
     );
 
     var formdata = new FormData();
-    formdata.append('docfile', fileList[0]);
-    formdata.append('docname', docName);
-    formdata.append('docnote', docNote);
-    formdata.append('is_public', 'True');
+    formdata.append("docfile", fileList[0]);
+    formdata.append("docname", docName);
+    formdata.append("docnote", docNote);
+    formdata.append("is_public", "True");
 
     var requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: myHeaders,
       body: formdata,
-      redirect: 'follow',
+      redirect: "follow",
     };
 
-    fetch('https://dev.api.coe.bmte.vn/dm/data/docs', requestOptions)
+    fetch("https://dev.api.coe.bmte.vn/dm/data/docs", requestOptions)
       .then((response) => response.text())
       .then((result) =>
         getDataDocument((res) => {
           if (res) {
-            message.success(`Create successfully.`);
+            message.success(t("document.createSuccess"));
             setSelected(res);
           }
         })
       )
-      .catch((error) => message.success(`Create successfully.`));
+      .catch((error) => message.success(t("document.createSuccess")));
   };
 
   const props = {
@@ -81,24 +81,24 @@ const Createfrom = ({ selected, getDataDocument, setSelected }) => {
   return (
     <FormWrapper>
       <>
-        <TitleUpdateDoc>{t('document.createDocument<')}</TitleUpdateDoc>
-        <Title>{t('document.documentName')}</Title>
+        <TitleUpdateDoc>{t("document.createDocument<")}</TitleUpdateDoc>
+        <Title>{t("document.documentName")}</Title>
         <Input
-          placeholder={t('document.documentName')}
-          className='width-300'
+          placeholder={t("document.documentName")}
+          className="width-300"
           value={docName}
           onChange={(e) => setDocName(e?.target?.value)}
         />
 
-        <Title>{t('document.documentNote')}</Title>
+        <Title>{t("document.documentNote")}</Title>
         <TextArea
-          placeholder='Document Note'
-          className='width-300'
+          placeholder={t("document.documentNote")}
+          className="width-300"
           value={docNote}
           onChange={(e) => setDocNote(e?.target?.value)}
         />
 
-        <Title>{t('document.uploadCertification')}</Title>
+        <Title>{t("document.uploadCertification")}</Title>
         <DraggerFile
           height={130}
           style={{ width: 300 }}
@@ -106,23 +106,21 @@ const Createfrom = ({ selected, getDataDocument, setSelected }) => {
           value={fileList}
           {...props}
         >
-          <p className='ant-upload-drag-icon'>
+          <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
-          <p className='ant-upload-text'>
-            {t("document.clickDrag")}
-          </p>
+          <p className="ant-upload-text">{t("document.clickDrag")}</p>
         </DraggerFile>
 
         <UploadWrapper>
           <Row>
             <ButtonCustom
-              type='primary'
+              type="primary"
               onClick={handleUpload}
               disabled={fileList.length === 0 || !docName || !docNote}
               loading={uploading}
             >
-              {uploading ? 'Uploading' : 'Save'}
+              {uploading ? t("document.uploading") : t("document.save")}
             </ButtonCustom>
           </Row>
         </UploadWrapper>
