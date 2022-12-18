b781ecd1747c47a52eaf15a5a0ba72314860acf8
Huynh.nguyen <>
2022-12-18 14:16:12 +0700

    login view

--- src/pages/apps/index.jsx
+++ src/pages/apps/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppsWrapper } from "./styled";
+import { AppsWrapper, LoginView } from "./styled";
 import { useNavigate } from "react-router-dom";
 import { Button, Modal } from "antd";
 import { useTranslation } from "react-i18next";
@@ -27,14 +27,17 @@ const AppsPage = () => {
         <img src={logo} alt="" />
       </div>
       <div className="item blue" onClick={() => navigate("/login")}>
+        <LoginView>{t("common.login")}</LoginView>
         <div className="title">{t("app.breastfeeding")}</div>
         <div className="hover-image-1" />
       </div>
       <div className="item yellow" onClick={showConfirm}>
+        <LoginView>{t("common.login")}</LoginView>
         <div className="title">{t("app.breastMilkBank")}</div>
         <div className="hover-image-2" />
       </div>
       <div className="item green" onClick={showConfirm}>
+        <LoginView>{t("common.login")}</LoginView>
         <div className="title">{t("app.nationalMinorities")}</div>
         <div className="hover-image-3" />
       </div>
